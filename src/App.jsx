901edd3ea6469934ae78190d94dc3377811427b1
Mercede Chen <mercedechen@gmail.com>
2023-12-16 08:09:27 -0500

    Render puppy data with a click handler

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,35 @@ import './App.css'
 function App() {
   
   const [puppies, setPuppies] = useState(puppyList)
+
+  // default value is set to null so that you can continually render an element in your JSX code
+  const [featPupId, setFeatPupId] = useState(null)
   
   console.log(puppies);
+  
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+  console.log(featuredPup);
 
   return (
     <>
       <div>
         {
           puppies.map((puppy) => {
-            return <p key={puppy.id}>{puppy.name}</p>
+            return <p onClick={() => {setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
         }
+
+        {/* {featPupId && <p>{featPupId}</p>} */}
+        {featPupId && (
+          <div>
+            <h2>{featuredPup.name}</h2>
+            <ul>
+              <li>Age: {featuredPup.age}</li>
+              <li>Email: {featuredPup.email}</li>
+            </ul>
+          </div>
+        )}
+
       </div>
     </>
   )
